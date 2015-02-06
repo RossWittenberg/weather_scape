c7ad21cf80668d7a309f8b0fb54910de20357805
RossWittenberg <ross.wittenberg@gmail.com>
2015-02-06 13:59:41 -0500

    moved images

--- app/assets/javascripts/tree.js
+++ app/assets/javascripts/tree.js
@@ -2,13 +2,13 @@ function drawTree(currentSeason){
 	var treeDiv = $('.treeDiv');
 	treeDiv.empty()
 	if (currentSeason === 'tropical'){
-		var palmTree = $('<img>').attr('src', 'assets/palm_tree.png')
+		var palmTree = $('<img>').attr('src', 'assets/palm_tree-a823538f31f348009b2acdcd65ccd19f.png')
 														 .attr('id', 'palmTree');
 		palmTree.appendTo(treeDiv);												 
 	} else if (currentSeason === 'arctic'){
 			return
 	} else {
-		var temperateTree = $('<img>').attr('src', 'assets/temperate_trees.png')
+		var temperateTree = $('<img>').attr('src', 'assets/temperate_trees-ddc857aa92a49a9ad3ca286497b3821d.png')
 														 .attr('id', 'temperateTrees');
 		temperateTree.appendTo(treeDiv);
 	};		
@@ -19,7 +19,7 @@ function drawLeaves(currentSeason){
 	var leavesDiv = $('#leaves');
 	leavesDiv.empty();
 	if (currentSeason === 'fall'){
-		var temperateTreeLeaves = $('<img>').attr('src', 'assets/temperate_tree_leaves_fall.png')
+		var temperateTreeLeaves = $('<img>').attr('src', 'assets/temperate_tree_leaves_fall-cb7f9ff9fce84d763e7f253ab047e218.png')
 														 .attr('id', 'temperateTreeLeaves');
 		temperateTreeLeaves.appendTo(leavesDiv)	
 	} else if ((currentSeason === 'spring') || (currentSeason === 'summer')) {
