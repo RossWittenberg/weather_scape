db648aad12c88ee860dd9c245d89a9932734091a
RossWittenberg <ross.wittenberg@gmail.com>
2015-02-08 12:37:19 -0500

    adjust image names for production

--- app/assets/javascripts/tree.js
+++ app/assets/javascripts/tree.js
@@ -2,14 +2,14 @@ function drawTree(currentSeason){
 	var treeDiv = $('.treeDiv');
 	treeDiv.empty()
 	if (currentSeason === 'tropical'){
-		var palmTree = $('<img>').attr('src', 'assets/palm_tree-a823538f31f348009b2acdcd65ccd19f.png')
-														 .attr('id', 'palmTree');
+		var palmTree = $('<img>').setAttribute('src', 'assets/palm_tree-a823538f31f348009b2acdcd65ccd19f.png')
+														 .setAttribute('id', 'palmTree');
 		palmTree.appendTo(treeDiv);												 
 	} else if (currentSeason === 'arctic'){
 			return
 	} else {
-		var temperateTree = $('<img>').attr('src', 'assets/temperate_trees-2b597ef0210d7c18af7db0465303bd12.png')
-														 .attr('id', 'temperateTrees');
+		var temperateTree = $('<img>').setAttribute('src', 'assets/temperate_trees-2b597ef0210d7c18af7db0465303bd12.png')
+														 .setAttribute('id', 'temperateTrees');
 		temperateTree.appendTo(treeDiv);
 	};		
 }
@@ -19,12 +19,12 @@ function drawLeaves(currentSeason){
 	var leavesDiv = $('#leaves');
 	leavesDiv.empty();
 	if (currentSeason === 'fall'){
-		var temperateTreeLeaves = $('<img>').attr('src', 'assets/temperate_tree_leaves_fall-cb7f9ff9fce84d763e7f253ab047e218.png')
-														 .attr('id', 'temperateTreeLeaves');
+		var temperateTreeLeaves = $('<img>').setAttribute('src', 'assets/temperate_tree_leaves_fall-cb7f9ff9fce84d763e7f253ab047e218.png')
+														 .setAttribute('id', 'temperateTreeLeaves');
 		temperateTreeLeaves.appendTo(leavesDiv)	
 	} else if ((currentSeason === 'spring') || (currentSeason === 'summer')) {
-		var temperateTreeLeaves = $('<img>').attr('src', 'assets/temperate_tree_leaves_summer-885f9ef4ea4231518b2c28375ee9351f.png')
-														 .attr('id', 'temperateTreeLeaves');
+		var temperateTreeLeaves = $('<img>').setAttribute('src', 'assets/temperate_tree_leaves_summer-885f9ef4ea4231518b2c28375ee9351f.png')
+														 .setAttribute('id', 'temperateTreeLeaves');
 		temperateTreeLeaves.appendTo(leavesDiv)	
 	} else{
 		return;
