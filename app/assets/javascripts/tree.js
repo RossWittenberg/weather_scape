335439c40a4b9b112adfa5f0e03e1999cc86255d
RossWittenberg <ross.wittenberg@gmail.com>
2015-02-09 16:17:24 -0500

    changed leaf pngs

--- app/assets/javascripts/tree.js
+++ app/assets/javascripts/tree.js
@@ -22,13 +22,13 @@ function drawLeaves(currentSeason){
 	leavesDiv.empty();
 	if (currentSeason === 'fall'){
 		var temperateTreeLeaves = $('<img>').attr({
-			src: 'assets/temperate_tree_leaves_fall-cb7f9ff9fce84d763e7f253ab047e218.png',
+			src: 'assets/images/temperate_tree_leaves_fall.png',
 			id: 'temperateTreeLeaves'
 		});
 		temperateTreeLeaves.appendTo(leavesDiv)	
 	} else if ((currentSeason === 'spring') || (currentSeason === 'summer')) {
 		var temperateTreeLeaves = $('<img>').attr({
-			src: 'assets/temperate_tree_leaves_summer-885f9ef4ea4231518b2c28375ee9351f.png',
+			src: 'assets/images/temperate_tree_leaves_summer.png',
 			id: 'temperateTreeLeaves'
 		});
 		temperateTreeLeaves.appendTo(leavesDiv)	
